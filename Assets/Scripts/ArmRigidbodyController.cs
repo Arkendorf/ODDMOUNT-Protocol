5b352f289f5663bff8950b177c192b7ee4a09c9f
Arkendorf <amon@ferri.us>
2022-04-20 16:43:47 -0400

    Rigidbody controller with deadzone (for mech arms)

---
+++ Assets/Scripts/ArmRigidbodyController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRigidbodyController : RigidbodyController
+{
+    [Space]
+    public Transform anchorParent;
+    public Vector3 anchorVector;
+    public float anchorRange;
+
+    private float delta = .1f;
+
+    // Update is called once per frame
+    protected override Vector3 GetTorque()
+    {
+        Vector3 torque = Vector3.zero;
+
+        if (target)
+        {
+            // Get current rotation, and goal rotation
+            Quaternion targetRotation = target.rotation;
+            Quaternion currentRotation = transform.rotation;
+
+            bool reverse = false;
+
+            if (anchorParent)
+            {
+                Vector3 anchorVector = anchorParent.rotation * this.anchorVector;
+
+                // Cap target rotation to outside the anchor vector forbidden zone
+                if (Vector3.Angle(targetRotation * Vector3.forward, anchorVector) <= anchorRange)
+                {
+                    targetRotation = Quaternion.LookRotation(Quaternion.AngleAxis(anchorRange, Vector3.Cross(anchorVector, targetRotation * Vector3.forward)) * anchorVector);
+                }
+
+                // Debug stuff
+                Debug.DrawLine(transform.position, transform.position + currentRotation * Vector3.forward * 2, Color.blue);
+                Debug.DrawLine(transform.position, transform.position + targetRotation * Vector3.forward * 2, Color.red);
+                Debug.DrawLine(transform.position, transform.position + anchorVector * 2, Color.black);
+
+                // Get angle between current and target
+                float deltaAngle = Vector3.Angle(currentRotation * Vector3.forward, targetRotation * Vector3.forward);
+
+                // Get offset
+                Vector3 axis = Vector3.Cross(currentRotation * Vector3.forward, targetRotation * Vector3.forward);
+                Vector3 right = Vector3.Cross(anchorVector, axis);
+                Vector3 unclampedAnchorVector = Vector3.Cross(axis, Vector3.Cross(anchorVector, axis));
+
+                // Clamp offset to range
+                float offsetAngle = Vector3.SignedAngle(anchorVector, unclampedAnchorVector, right);
+                offsetAngle = Mathf.Clamp(offsetAngle, -anchorRange, anchorRange);
+
+                // Adjust anchor vector by offset
+                Vector3 offsetAnchorVector = Quaternion.AngleAxis(offsetAngle, right) * anchorVector;
+
+                // Angle between current and target, crossing through clamped anchor vector
+                float crossAngle = Vector3.Angle(currentRotation * Vector3.forward, offsetAnchorVector) + Vector3.Angle(offsetAnchorVector, targetRotation * Vector3.forward);
+
+                // Check if angle is crossing through forbidden anchor vector zone
+                reverse = crossAngle - delta <= deltaAngle;
+            }
+
+            // Calculate torque (only one alignment should reverse at a time)
+            if (alignForward)
+                torque += AlignVectors(currentRotation * Vector3.forward, targetRotation * Vector3.forward, reverse);
+
+            if (alignUp && !alignForward && !alignRight)
+                torque += AlignVectors(currentRotation * Vector3.up, targetRotation * Vector3.up, reverse);
+            else if (alignUp)
+                torque += AlignVectors(currentRotation * Vector3.up, targetRotation * Vector3.up);
+
+            if (alignRight && !alignForward)
+                torque += AlignVectors(currentRotation * Vector3.right, targetRotation * Vector3.right, reverse);
+            else if (alignRight)
+                torque += AlignVectors(currentRotation * Vector3.right, targetRotation * Vector3.right);
+        }
+        return torque;
+    }
+}
--- Assets/Scripts/RigidbodyController.cs
+++ Assets/Scripts/RigidbodyController.cs
@@ -26,7 +26,7 @@ public class RigidbodyController : MonoBehaviour
     [Range(0, 1)]
     public float gravityResistance = 0;
 
-    private new Rigidbody rigidbody;
+    protected new Rigidbody rigidbody;
 
     public delegate void CollisionEvents(Collision collision);
     public event CollisionEvents CollisionEnter;
@@ -56,8 +56,29 @@ public class RigidbodyController : MonoBehaviour
 
     public void CorrectPosition()
     {
-        // damping prevents overshooting the target position
-        Vector3 force = -moveDamping * rigidbody.velocity;
+        Vector3 force = GetForce();
+
+        // Add damping
+        force += -moveDamping * rigidbody.velocity; 
+
+        // Correct against gravity
+        if (rigidbody.useGravity)
+        {
+            force -= rigidbody.mass * Physics.gravity * gravityResistance;
+        }
+
+        // Cap force
+        if (force.sqrMagnitude > maxForce * maxForce)
+        {
+            force = force.normalized * maxForce;
+        }
+
+        rigidbody.AddForce(force);
+    }
+
+    protected virtual Vector3 GetForce()
+    {
+        Vector3 force = Vector3.zero;
 
         Vector3 delta = Vector3.zero;
         if (target)
@@ -79,29 +100,33 @@ public class RigidbodyController : MonoBehaviour
                 delta.z = 0;
                 force.z = 0;
             }
-        }          
+        }
 
         force += delta * moveSpeed;
 
-        // Correct against gravity
-        if (rigidbody.useGravity)
-        {
-            force -= rigidbody.mass * Physics.gravity * gravityResistance;
-        }
-
-        // Cap force
-        if (force.sqrMagnitude > maxForce * maxForce)
-        {
-            force = force.normalized * maxForce;
-        }
-
-        rigidbody.AddForce(force);
+        return force;
     }
 
     public void CorrectRotation()
     {
-        // damping prevents overshooting the target rotation
-        Vector3 torque = -rotateDamping * rigidbody.angularVelocity;
+        Vector3 torque = GetTorque();
+
+        // Add damping
+        torque += -rotateDamping * rigidbody.angularVelocity;
+
+        // Cap torque
+        if (torque.sqrMagnitude > maxTorque * maxTorque)
+        {
+            torque = torque.normalized * maxTorque;
+        }
+
+        // Apply torque
+        rigidbody.AddTorque(torque);
+    }
+
+    protected virtual Vector3 GetTorque()
+    {
+        Vector3 torque = Vector3.zero;
 
         // Align directions
         if (target)
@@ -114,21 +139,25 @@ public class RigidbodyController : MonoBehaviour
                 torque += AlignVectors(transform.right, target.right);
         }
 
-        // Cap torque
-        if (torque.sqrMagnitude > maxTorque * maxTorque)
-        {
-            torque = torque.normalized * maxTorque;
-        }
-
-        // Apply torque
-        rigidbody.AddTorque(torque);
+        return torque;
     }
 
-    private Vector3 AlignVectors(Vector3 current, Vector3 goal)
+    //protected Vector3 AlignVectors(Vector3 current, Vector3 goal, bool reverse = false)
+    //{
+    //    Quaternion delta = Quaternion.FromToRotation(current, goal);
+    //    delta.ToAngleAxis(out float angle, out Vector3 axis);
+    //    return axis.normalized * angle * rotateSpeed;
+    //}
+
+    protected Vector3 AlignVectors(Vector3 current, Vector3 goal, bool reverse = false)
     {
-        // Align y
-        Quaternion delta = Quaternion.FromToRotation(current, goal);
-        delta.ToAngleAxis(out float angle, out Vector3 axis);
+        Vector3 axis = Vector3.Cross(current, goal);
+        float angle = Vector3.Angle(current, goal);
+        if (reverse)
+        {
+            angle = (360 - angle) % 360;
+            axis *= -1;
+        }
         return axis.normalized * angle * rotateSpeed;
     }
 }
